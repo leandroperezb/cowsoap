8e389bbe324ad8a45cb963c44c3146e1e822a653
Leandro <leandroperezb@gmail.com>
2019-09-18 16:01:54 -0300

    Dockerfile

--- src/main/java/org/example/cows/AddCowToHerdRequest.java
+++ src/main/java/org/example/cows/AddCowToHerdRequest.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2019.09.18 a las 03:18:58 PM ART 
+// Generado el: 2019.09.18 a las 03:52:05 PM ART 
 //
 
 
